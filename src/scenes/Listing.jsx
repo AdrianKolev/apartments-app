34aa9e6366ea6d78677b49d1ca8b64c62f19726e
Adrian Kolev <adrian.kolev@netea.bg>
2022-08-11 15:12:51 +0300

    adding slider with images to the listing

--- src/scenes/Listing.jsx
+++ src/scenes/Listing.jsx
@@ -1,15 +1,20 @@
-import {useEffect, useState} from "react";
-import {Link, useNavigate, useParams} from "react-router-dom";
-import {doc, getDoc} from "firebase/firestore";
-import {getAuth} from "firebase/auth";
-import {db} from '../firebase.config';
-import Spinner from "../components/Spinner";
-import shareIcon from '../assets/svg/shareIcon.svg';
+import { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+import { getDoc, doc } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { db } from '../firebase.config'
+import Spinner from '../components/Spinner'
+import shareIcon from '../assets/svg/shareIcon.svg'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [sharedLinkCopied, setSharedLinkCopied] = useState(false);
+    const [shareLinkCopied, setShareLinkCopied] = useState(false)
 
     const navigate = useNavigate();
     const params = useParams();
@@ -36,16 +41,33 @@ function Listing() {
 
     return (
         <main>
-            <div className="shareIconDiv" onClick={() => {
-                navigator.clipboard.writeText(window.location.href)
-                setSharedLinkCopied(true);
-                setTimeout(() => {
-                    setSharedLinkCopied(false)
-                }, 2000)
-            }}>
-                <img src={shareIcon}/>
+            <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+                {listing.imageUrls.map((url, index) => (
+                    <SwiperSlide key={index}>
+                        <div
+                            style={{
+                                background: `url(${listing.imageUrls[index]}) center no-repeat`,
+                                backgroundSize: 'cover',
+                            }}
+                            className='swiperSlideDiv'
+                        ></div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+
+            <div
+                className='shareIconDiv'
+                onClick={() => {
+                    navigator.clipboard.writeText(window.location.href)
+                    setShareLinkCopied(true)
+                    setTimeout(() => {
+                        setShareLinkCopied(false)
+                    }, 2000)
+                }}
+            >
+                <img src={shareIcon} alt='' />
             </div>
-            {sharedLinkCopied && <p className="linkCopied">Link Copied</p>}
+            {shareLinkCopied && <p className="linkCopied">Link Copied</p>}
             <div className="listingDetails">
                 <p className='listingName'>
                     {listing.name} - $
@@ -86,6 +108,25 @@ function Listing() {
 
                 <p className="listingLocationTitle">Location</p>
 
+                <div className='leafletContainer'>
+                    <MapContainer
+                        style={{ height: '100%', width: '100%' }}
+                        center={[listing.geolocation.lat, listing.geolocation.lng]}
+                        zoom={13}
+                        scrollWheelZoom={false}
+                    >
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+                        />
+
+                        <Marker
+                            position={[listing.geolocation.lat, listing.geolocation.lng]}
+                        >
+                            <Popup>{listing.location}</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
 
                 {auth.currentUser?.uid !== listing.userRef && (
                     <Link
